8ff6bbad50958f9efb1462f713ec094ec0dc4de7
Ayaan-apk <ayaanghosh5@gmail.com>
2021-07-21 20:51:26 +0530

    Api

---
+++ app/src/main/java/com/example/restapi/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.restapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    ApiInterface apiInterface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        apiInterface = Retrofitinstance.getRetrofit().create(ApiInterface.class);
+        apiInterface.getpost().enqueue(new Callback<List<PostPojo>>() {
+            @Override
+            public void onResponse(Call<List<PostPojo>> call, Response<List<PostPojo>> response) {
+                if (response.body().size()>0){
+                    Toast.makeText(MainActivity.this,"List is not empty!",Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"List is empty!",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<PostPojo>> call, Throwable t) {
+                Toast.makeText(MainActivity.this,t.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
